1136e82d2119d33ac16c8f5681dcf387365e4741
Mikkel <mikkel@mediagram.dk>
2017-08-30 17:56:14 +0200

    wip

--- dom_manipulation_events.js
+++ dom_manipulation_events.js
@@ -63,12 +63,12 @@ body[0].insertAdjacentHTML("afterbegin", "<p id='myP'></p>");
 var myP = document.getElementById("myP");
 
 // 4
+
+// a
 body[0].insertAdjacentHTML("afterbegin", "<div id='magicList'></div>");
 var magicList = document.getElementById("magicList");
 
 var names = ["Joe", "Susan", "Peter", "Jack"];
-
-magicList.innerHTML += "<ul>" + listItems(names) + "</ul>";
 
 function listItems(array) {
     var result = "";
@@ -78,4 +78,95 @@ function listItems(array) {
     return result;
 }
 
-// 
+magicList.innerHTML += "<ul>" + listItems(names) + "</ul>";
+
+// b
+magicList.insertAdjacentHTML("afterend", "<form><input id='nameTextField' value=''><button id='nameTextButton'>Add name</button><button id='removeFirstButton'>Remove first</button><button id='removeLastButton'>Remove last</button></form>");
+var nameTextField = document.getElementById("nameTextField");
+var nameTextButton = document.getElementById("nameTextButton");
+
+function addNameToList(e) {
+    names.push(nameTextField.value);
+    magicList.innerHTML = "";
+    magicList.innerHTML += "<ul>" + listItems(names) + "</ul>";
+    e.preventDefault();
+}
+
+nameTextButton.addEventListener("click", addNameToList);
+
+// c
+function removeFirst(e) {
+    names.shift();
+    magicList.innerHTML = "";
+    magicList.innerHTML += "<ul>" + listItems(names) + "</ul>";
+    e.preventDefault();
+}
+
+function removeLast(e) {
+    names.pop();
+    magicList.innerHTML = "";
+    magicList.innerHTML += "<ul>" + listItems(names) + "</ul>";
+    e.preventDefault();
+}
+
+var removeFirstButton = document.getElementById("removeFirstButton");
+var removeLastButton = document.getElementById("removeLastButton");
+
+removeFirstButton.addEventListener("click", removeFirst);
+removeLastButton.addEventListener("click", removeLast);
+
+// 5
+body[0].insertAdjacentHTML("afterbegin", "<div id='carListContainer'></div>");
+var carListContainer = document.getElementById("carListContainer");
+
+var cars = [
+    { id: 1, year: 1997, make: 'Ford', model: 'E350', price: 3000 },
+    { id: 2, year: 1999, make: 'Chevy', model: 'Venture', price: 4900 },
+    { id: 3, year: 2000, make: 'Chevy', model: 'Venture', price: 5000 },
+    { id: 4, year: 1996, make: 'Jeep', model: 'Grand Cherokee', price: 4799 },
+    { id: 5, year: 2005, make: 'Volvo', model: 'V70', price: 44799 }
+  ];
+  
+// a
+function printCarTable(array) {
+    carListContainer.innerHTML += "<div class='table' id='t_container'></div";
+    var tableContainer = document.getElementById("t_container");
+
+    for (var i = 0; i < array.length; i++) {
+        tableContainer.innerHTML += 
+        "<div class='table-row'><div class='table-cell'>" + array[i].id + "</div>" +
+        "<div class='table-cell'>" + array[i].year + "</div>" +
+        "<div class='table-cell'>" + array[i].make + "</div>" +
+        "<div class='table-cell'>" + array[i].model + "</div>" +
+        "<div class='table-cell'>" + array[i].price + "</div></div>";
+    }
+}
+
+printCarTable(cars);
+
+// b
+carListContainer.insertAdjacentHTML("afterend", "<form><input id='carTextField' value=''><button id='carTextButton'>Price less than</button></form>");
+var carTextField = document.getElementById("carTextField");
+var carTextButton = document.getElementById("carTextButton");
+
+function filterCars(car) {
+    var myNewCarArray = [];
+    if (car.year < 1999) {
+        myNewCarArray.push(this);
+
+        return myNewCarArray;
+    }
+}
+
+function sortCars(e) {
+    var newCarList = cars.filter(filterCars);
+    carListContainer.innerHTML = "";
+    printCarTable(newCarList);
+    e.preventDefault();
+}
+
+carTextButton.addEventListener("click", sortCars);
+
+// 6
+
+
